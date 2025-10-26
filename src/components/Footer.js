7c87f229fc60d5488f7cfaa28b3f55a7f17de396
priya-dharshini-t <priyadharshinipriya1975@gmail.com>
2025-10-26 22:14:57 +0530

    final commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer">
-      © {new Date().getFullYear()} Priya Dharshini T — All Rights Reserved.
+      © {new Date().getFullYear()} Priya Dharshini T 
     </footer>
   );
 }
